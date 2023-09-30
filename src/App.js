26a6b7666da2ff0544de47fe66d3e93e6c6f66af
Jamie Beasley <info@jamiebeas.com>
2023-09-30 16:36:00 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // We'll create this file shortly
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Outreach Dashboard</h1>
       </header>
+      <main>
+        <div className="prospect-card">
+          <h2>John Doe</h2>
+          <p>Company: ABC Corp</p>
+          <p>Likelihood to Buy: High</p>
+        </div>
+      </main>
+
     </div>
   );
 }
